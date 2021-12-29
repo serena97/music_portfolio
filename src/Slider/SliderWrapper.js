399c1109b3fe274144194cea1b2e4c19b84c7d28
Serena <serena_chan@outlook.com>
2021-12-29 22:06:59 +0000

    configured slider up and down button

--- src/Slider/SliderWrapper.js
+++ src/Slider/SliderWrapper.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './Slider.css'
 
-import FluidGallery from 'react-fluid-gallery'
+import FluidGallery, { changeSlideEnum } from 'react-fluid-gallery'
 
 import img1 from '../assets/image1.jpeg';
 import img2 from '../assets/image2.jpeg';
@@ -11,8 +11,13 @@ export default class SliderWrapper extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      changeSlide: undefined
+    }
     this.width = null;
     this.setSliderWidth = this.setSliderWidth.bind(this)
+    this.onClickNext = this.onClickNext.bind(this);
+    this.onClickPrev = this.onClickPrev.bind(this);
   }
 
   setSliderWidth() {
@@ -29,10 +34,25 @@ export default class SliderWrapper extends Component {
     })
   }
 
+  onClickPrev = () => {
+    this.setState({changeSlide: changeSlideEnum.prev});
+  }
+
+  onClickNext = () => {
+    this.setState({changeSlide: changeSlideEnum.next});
+  }
+
   render () {
     return (
       <div className='slider'>
+        <button type="button" class='arrow arrow-up btn btn-lg' onClick={this.onClickNext}>
+          <span class="glyphicon glyphicon-arrow-up"></span>
+        </button>
+        <button type="button" class='arrow arrow-down btn btn-lg' onClick={this.onClickPrev}>
+          <span class="glyphicon glyphicon-arrow-down"></span>
+        </button>
         <FluidGallery
+          changeSlide={this.state.changeSlide}
           style={{ height: '100vh'}}
           slides={[ img1, img2, img3]}
         />
