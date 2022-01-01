83b7245c22ebb735f7c823759fb80a728b4f00e8
Serena <serena_chan@outlook.com>
2022-01-01 19:15:09 +0000

    add blog menu

--- src/Pillar/Pillar.js
+++ src/Pillar/Pillar.js
@@ -7,6 +7,9 @@ export default function Pillar() {
               Ab<span className="strike">o</span>ut
             </div>
             <div className='nav-title'>
+              Bl<span className="strike">o</span>g
+            </div>
+            <div className='nav-title'>
               C<span className="strike">o</span>ntact
             </div>
         </div>
